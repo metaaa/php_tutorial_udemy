7bf3eaa79db32c1781dd61d2f35c279aaf78c970
root <root@metaaa.metadoesprog.com>
2018-10-15 22:09:48 +0200

    switch statement

---
+++ switch/switch.php
@@ -0,0 +1,15 @@
+<?php
+$number = 0; //value
+echo "the number is: $person <br>";
+switch ($number) {
+	case $number > 0:
+		echo 'positive';
+		break;
+	case $number < 0:
+		echo 'negative';
+		break;
+	default:
+		echo 'zero';
+		break;
+}
+?>
