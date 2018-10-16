fadc8b6614ebf000bd79fe8897a05fd1abf47a41
root <root@metaaa.metadoesprog.com>
2018-10-16 18:43:30 +0200

    switch statement modified

--- switch/switch.php
+++ switch/switch.php
@@ -1,15 +1,15 @@
 <?php
-$name = 'Frank'; 						//declaration of the variable
-echo ('the name is: '.$name.'<br>'); 	//prints out the variable
-switch ($name) {						//the investigated variable
-	case $name == 'Sarah':				//condition
-		echo 'it\'s Sarah'; 				//if true, it will print out the text
-		break; 								//breaks the investigation - no further comparisons will be made
-	case $name == 'Jack': 				//condition
-		echo 'it\'s Jack'; 					//if true, it will print out the text
-		break;								//breaks the investigation - no further comparisons will be made
-	default: 							//in any other case do this
-		echo 'it\'s nobody'; 			//it will print out the text if all of the upper comparisons get false
+$name = 'Frank'; //declaration of the variable
+echo ('the name is: '.$name.'<br>'); //prints out the variable
+switch ($name) { //the investigated variable
+	case $name == 'Sarah': //condition
+		echo 'it\'s Sarah'; //if true, it will print out the text
+		break; 	//breaks the investigation - no further comparisons will be made
+	case $name == 'Jack': //condition
+		echo 'it\'s Jack'; //if true, it will print out the text
+		break; //breaks the investigation - no further comparisons will be made
+	default: //in any other case do this
+		echo 'it\'s nobody'; //it will print out the text if all of the upper comparisons get false
 		break;
 }
 ?>
